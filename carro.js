76b5a46a98e88338ec3ce6bc70ccb0c74e08ef89
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-24 10:46:32 -0300

    dividindo codigos em arquirvos diferentes

---
+++ carro.js
@@ -0,0 +1,12 @@
+//carros
+let xCarro = 600;
+let yCarro = 40;
+
+function mostrarCarro() {
+    image(imagemCarro, xCarro, yCarro, 50, 40)
+  }
+  
+  function movimentaCarro() {
+    xCarro -= 2;
+  }
+  
--- sketch.js
+++ sketch.js
@@ -1,18 +1,3 @@
-let imagemDaEstrada;
-let imagemDoAtor;
-let imagemCarro;
-//carros
-let xCarro = 600;
-
-// ator
-let yAtor = 366;
-
-function preload() {
-  imagemDaEstrada = loadImage("imagens/estrada.png");
-  imagemDoAtor = loadImage("imagens/ator-1.png");
-  imagemCarro = loadImage("imagens/carro-1.png")
-}
-
 function setup() {
   createCanvas(500, 400);
 }
@@ -25,23 +10,3 @@ function draw() {
   movimentaAtor();
 }
 
-function mostrarAtor() {
-  image(imagemDoAtor, 100, yAtor, 30, 30);
-}
-
-function mostrarCarro() {
-  image(imagemCarro, xCarro, 40, 50, 40)
-}
-
-function movimentaCarro() {
-  xCarro -= 2;
-}
-
-function movimentaAtor() {
-  if (keyIsDown(UP_ARROW)) {
-    yAtor -= 3;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    yAtor += 3;
-  }
-}
