c998cd636beadf048db5ac4dc9edce04a561e0c1
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-27 14:18:16 -0300

    resolvendo conflito de codigo

--- carro.js
+++ carro.js
@@ -4,26 +4,26 @@ let xCarros = [600, 600, 600];
 let yCarros = [40, 96, 150];
 let velocidadeCarros = [2, 2.5, 3.2];
 
-function mostraCarro() {
-    image(imagemCarros[0], xCarros[0], yCarros[0], 50, 40);
-    image(imagemCarros[1], xCarros[1], yCarros[1], 50, 40);
-    image(imagemCarros[2], xCarros[2], yCarros[2], 50, 40);
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i = i + 1){
+    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
+  }
 }
 
-function movimentaCarro() {
-    for (let i = 0; i < imagemCarros.length; i = i + 1) {
-        xCarros[i] -= velocidadeCarros[i];
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i = i + 1){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for (let i = 0; i < imagemCarros.length; i = i + 1){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
     }
+  }
 }
 
-function voltaPosicaoInicialDoCarro() {
-    for (let i = 0; i < imagemCarros.length; i = i + 1) {
-        if (passouTodaAtela(xCarros[i])) {
-            xCarros[i] = 600;
-        }
-    }
+function passouTodaATela(xCarro){
+  return xCarro < - 50;
 }
-
-function passouTodaAtela(xCarros) {
-    return xCarros[0] < -50;
-}  
