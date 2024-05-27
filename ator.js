d24405662897e44da4c0f6b7cac45780487ff674
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-27 15:03:18 -0300

    adicionando funçoes para marcar ponto e voltar ator a pocisao inicial

--- ator.js
+++ ator.js
@@ -1,17 +1,18 @@
 //código do ator
-let xAtor = 100;
+let xAtor = 85;
 let yAtor = 366;
 let colisao = false;
+let meusPontos = 0;
 
-function mostraAtor(){
+function mostraAtor() {
   image(imagemDoAtor, xAtor, yAtor, 30, 30);
 }
 
-function movimentaAtor(){
-  if (keyIsDown(UP_ARROW)){
+function movimentaAtor() {
+  if (keyIsDown(UP_ARROW)) {
     yAtor -= 3;
   }
-  if (keyIsDown(DOWN_ARROW)){
+  if (keyIsDown(DOWN_ARROW)) {
     yAtor += 3;
   }
 }
@@ -21,11 +22,26 @@ function verificaColisao() {
   for (let i = 0; i < imagemCarros.length; i++) {
     colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
     if (colisao) {
-      colidiu();
+      voltaAtorPosicaoInicial();
     }
   }
 }
 
-function colidiu() {
+function voltaAtorPosicaoInicial() {
   yAtor = 366;
+}
+
+function incluirPontos() {
+  textAlign(CENTER);
+  fill(color(255, 255, 60));
+  textSize(25);
+  text(meusPontos, width / 5, 27);
+}
+
+function marcarPonto() {
+  if (yAtor < 15) {
+    meusPontos += 1;
+    voltaAtorPosicaoInicial();
+  }
+
 }
