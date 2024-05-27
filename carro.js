7191d81bad062f16825a5882977a82a4c3fdff2e
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-27 13:57:35 -0300

    criando lista de imagens

--- carro.js
+++ carro.js
@@ -1,13 +1,13 @@
 //código do carro
 
 let xCarros = [600, 600, 600];
-let yCarros = [40, 96, 150];
+let yCarros = [40, 96, 150]; 
 let velocidadeCarros = [2, 2.5, 3.2];
 
 function mostraCarro(){
-  image(imagemCarro, xCarros[0], yCarros[0], 50, 40);
-  image(imagemCarro2, xCarros[1], yCarros[1], 50, 40);
-  image(imagemCarro3, xCarros[2], yCarros[2], 50, 40);
+  image(imagemCarros[0], xCarros[0], yCarros[0], 50, 40);
+  image(imagemCarros[1], xCarros[1], yCarros[1], 50, 40);
+  image(imagemCarros[2], xCarros[2], yCarros[2], 50, 40);
 }
 
 function movimentaCarro(){
