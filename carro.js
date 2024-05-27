4d96a985ef14e5b1b3734089263c93a9ced2fe10
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-27 14:11:18 -0300

    refatorando e diminuindo codigo usando lista

--- carro.js
+++ carro.js
@@ -1,29 +1,29 @@
 //código do carro
 
 let xCarros = [600, 600, 600];
-let yCarros = [40, 96, 150]; 
+let yCarros = [40, 96, 150];
 let velocidadeCarros = [2, 2.5, 3.2];
 
-function mostraCarro(){
-  image(imagemCarros[0], xCarros[0], yCarros[0], 50, 40);
-  image(imagemCarros[1], xCarros[1], yCarros[1], 50, 40);
-  image(imagemCarros[2], xCarros[2], yCarros[2], 50, 40);
+function mostraCarro() {
+    image(imagemCarros[0], xCarros[0], yCarros[0], 50, 40);
+    image(imagemCarros[1], xCarros[1], yCarros[1], 50, 40);
+    image(imagemCarros[2], xCarros[2], yCarros[2], 50, 40);
 }
 
-function movimentaCarro(){
-  xCarros[0] -= velocidadeCarros[0];
-  xCarros[1] -= velocidadeCarros[1];
-  xCarros[2] -= velocidadeCarros[2];
+function movimentaCarro() {
+    for (let i = 0; i < imagemCarros.length; i = i + 1) {
+        xCarros[i] -= velocidadeCarros[i];
+    }
 }
 
-function voltaPosicaoInicialDoCarro(){
-  if (xCarros[0] < -50){
-    xCarros[0] = 600
-  }
-  if (xCarros[1] < -50){
-    xCarros[1] = 600
-  }
-  if (xCarros[2] < -50){
-    xCarros[2] = 600
-  }
+function voltaPosicaoInicialDoCarro() {
+    for (let i = 0; i < imagemCarros.length; i = i + 1) {
+        if (passouTodaAtela(xCarros[i])) {
+            xCarros[i] = 600;
+        }
+    }
 }
+
+function passouTodaAtela(xCarros) {
+    return xCarros[0] < -50;
+}  
